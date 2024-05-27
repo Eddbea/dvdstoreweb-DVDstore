fc7b201b8bc2e1fe8726d69705ba1c3a318dcfd4
Edouard Leroy <leroyedouard@protonmail.com>
2024-05-27 17:15:30 +0200

    tp32 Termine et STABLE

--- src/main/java/com/mycompany/dvdstore/dvdstoreweb/form/MovieForm.java
+++ src/main/java/com/mycompany/dvdstore/dvdstoreweb/form/MovieForm.java
@@ -1,7 +1,8 @@
 package com.mycompany.dvdstore.dvdstoreweb.form;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 public class MovieForm {
 
