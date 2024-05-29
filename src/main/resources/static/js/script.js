178fcb9c34cf0ae3ee9846460781de4ef2418d4b
Edouard Leroy <leroyedouard@protonmail.com>
2024-05-29 13:20:45 +0200

    tp33.2 Termine et STABLE dans sa litterature

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -64,14 +64,14 @@ function showDetail(id){
         console.log(res.description);
 
         p = document.createElement("p");
-        text = document.createTextNode('Main actor: ${res.mainActor.firstName} ${res.mainActor.lastName}');
+        text = document.createTextNode(`Main actor: ${res.mainActor.firstName} ${res.mainActor.lastName}`);
         p.appendChild(text)
         invoiceDetailNode.appendChild(p);
 
         p = document.createElement("p");
         var sum = res.reviews.map((a)=> a.mark).reduce((a,b) => a + b, 0);
         var avg = (sum/res.reviews.length) || 0;
-        text = document.createTextNode('Mark: ${avg}/5');
+        text = document.createTextNode(`Mark: ${avg}/5`);
         p.appendChild(text);
         invoiceDetailNode.appendChild(p);
 
